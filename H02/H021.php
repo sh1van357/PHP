fb0658700c0102ea8039d1b8b03a61aa532c1134
Shivan <Shivanbadloe@gmail.com>
2021-06-14 16:48:21 +0200

    update H2

--- H02/H021.php
+++ H02/H021.php
@@ -4,6 +4,15 @@ $a = "ik heb niet gefaald";
 $b = "die niet werken";
 $c = "10000";
 
-$volledigezin = $a . " ik heb alleen " . $c . " manieren gevonden " . $b;
+$volledigezin = $a . " ik heb alleen " . $c . " manieren gevonden " . $b . "<br>";
+
+echo $volledigezin;
+
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+$volledigezin = " mens" . $b . " " . $a . " mak" . $b . " , werk" . $b . " " . $c;
 
 echo $volledigezin;
